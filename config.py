1f3b6c66adc670f85cdfa2908aa43023f8538ef1
Aspekt112 <Aspekt112@gmail.com>
2016-10-27 23:54:59 +0300

    Fixed styles and coding

--- config.py
+++ config.py
@@ -1,1 +1,3 @@
+# -*- coding: utf-8 -*-
+
 token = '280519474:AAHm-iwyzVNUp2owOfJymi5_wJBbe3a99jw'
--- main.py
+++ main.py
@@ -1,37 +1,40 @@
+# -*- coding: utf-8 -*-
+
 import config
 import telebot
 import keyboards
 
 bot = telebot.TeleBot(config.token)
 
+
 class Place:
-    def __init__(self, Name, Lon, Lat, Type):
-        self.Name = Name
-        self.Type =['Кафэ', 'Ресторан','Фастфуд','Музей','Монумент','Древности'] #Доделать Класс
-        self.Lon = Lon
-        self.Lat = Lat
-        self.Rating = 0
+    def __init__(self, name, lon, lat, type):
+        self.name = name
+        self.type = ['Кафэ', 'Ресторан', 'Фастфуд', 'Музей', 'Монумент', 'Древности']  # Доделать Класс
+        self.lon = lon
+        self.lat = lat
+        self.rating = 0
 
     def __str__(self):
-        return 'Name:{0} Type:{1} Rating:{2}'.format(self.Name, self.Type,self.Rating)
+        return 'Name:{0} Type:{1} Rating:{2}'.format(self.name, self.type,self.rating)
 
 
 @bot.message_handler(commands=['start', 'help'])
-def printhelp(message):
-    bot.send_message(message.chat.id, keyboards.first_Msg, reply_markup=keyboards.first_Msg_Keyboard)
+def print_help(message):
+    bot.send_message(message.chat.id, keyboards.first_msg, reply_markup=keyboards.first_msg_keyboard)
 
 
 @bot.message_handler(content_types=["location"])
-def msgLocation(message):
-    bot.send_message(message.chat.id, message.location)
-    bot.send_message(message.chat.id, keyboards.second_Msg, reply_markup=keyboards.second_Msg_Keyboard)
-   # bot.send_message(message.chat.id, keyboards.third_Msg, reply_markup=keyboards.first_Msg_Keyboard)
+def msg_location(message):
+    bot.send_message(message.chat.id, message.location)  # Вывод коордианты {longitude, latitude}
+    bot.send_message(message.chat.id, keyboards.second_msg, reply_markup=keyboards.second_msg_keyboard)
+    # bot.send_message(message.chat.id, keyboards.third_Msg, reply_markup=keyboards.first_Msg_Keyboard)
 
 
 @bot.message_handler(content_types="text")
-def msgType(message):
-    bot.send_message(message.chat.id, 'Тип: {0}'.format(message.text)) #Если не подходит под Type класса сделать вывод сообщения
+def msg_type(message):  # Если не подходит под Type класса сделать вывод сообщения
+    bot.send_message(message.chat.id, 'Тип: {0}'.format(message.text))
 
 
 if __name__ == '__main__':
-     bot.polling(none_stop=True)
+    bot.polling(none_stop=True)
